fa1fd982e1afa29daeace8eae334de960d280be5
Andrea <s233799@studenti.polito.it>
2019-06-26 16:54:55 +0200

    Nuove query

--- src/scheletri/Query.java
+++ src/scheletri/Query.java
@@ -1,3 +1,5 @@
+//VERTICI
+
 //Selezionare tutti gli eventi criminosi in un determinato distretto in quel determinato anno
 "SELECT * FROM EVENTS WHERE is_crime=1 AND district_id=? AND YEAR(reported_date)=?"
 
@@ -19,18 +21,32 @@
 //Selezionare tutti gli eventi riportati in quell'anno, quel mese e quel giorno
 "SELECT * FROM events WHERE Year(reported_date)=? AND Month(reported_date)=? AND Day(reported_date)=?"
 
-//Selezionare diversi tipi di eventi criminosi
-"SELECT DISTINCT e.offense_type_id FROM events e WHERE e.is_crime = 1" ;
+//Selezionare diversi tipi di eventi criminosi o non
+"SELECT DISTINCT e.offense_type_id FROM events e WHERE e.is_crime = 1/0" ;
 
-//Selezionare diversi tipi di eventi criminosi nell'anno x
-"SELECT DISTINCT e.offense_type_id FROM events e WHERE e.is_crime = 1 AND year(e.reported_date) = ?";
+//Selezionare diversi tipi di eventi criminosi o non nell'anno x
+"SELECT DISTINCT e.offense_type_id FROM events e WHERE e.is_crime = 1/0 AND year(e.reported_date) = ?";
 
-//Selezionare distinct offense_code di eventi criminosi
-"SELECT DISTINCT e.offense_code FROM EVENTS e WHERE e.is_crime = 1";
+//Selezionare distinct offense_code di eventi criminosi o non
+"SELECT DISTINCT e.offense_code FROM EVENTS e WHERE e.is_crime = 1/0";
 
-//Selezionare distinct precinct_id di eventi criminosi
-"SELECT DISTINCT e.precinct_id FROM events e WHERE e.is_crime = 1";
+//Selezionare distinct precinct_id di eventi criminosi o non
+"SELECT DISTINCT e.precinct_id FROM events e WHERE e.is_crime = 1/0";
 
-//Selezionare distinct neighborhood_id di eventi criminosi
-"SELECT DISTINCT e.neighborhood_id FROM events e WHERE e.is_crime = 1";
+//Selezionare distinct neighborhood_id di eventi criminosi o non 
+"SELECT DISTINCT e.neighborhood_id FROM events e WHERE e.is_crime = 1/0";
 
+//Selezionare eventi criminosi o non  offense_code/precinct_id/neighborhood_id/offense_type_id = x, nell' anno y e distretto z
+"SELECT * FROM events e WHERE e.offense_code/precinct_id/neighborhood_id/offense_type_id = ? AND YEAR(e.reported_date) = ? AND e.district_id = ? AND e.is_crime = 1/0";
+
+//Seleziona eventi di tipo all-other-crimes con offense_code = x
+"SELECT * FROM EVENTS e WHERE e.offense_category_id = 'all-other-crimes' AND e.offense_code = ?"
+
+
+//ARCHI
+
+//Selezionare id,lat,lon di due eventi criminosi o meno avvenuti nello stesso giorno del mese dell'anno
+"SELECT e.incident_id AS e1,e.geo_lat AS lat1, e.geo_lon AS lon1, e2.geo_lat AS lat2, e2.geo_lon AS lon2 ,e2.incident_id AS e2 FROM events e , events e2 WHERE YEAR(e.reported_date)=? AND MONTH(e.reported_date)=? AND DAY(e.reported_date)=? AND YEAR(e2.reported_date)=? AND MONTH(e2.reported_date)=? AND DAY(e2.reported_date)=? AND e.incident_id <> e2.incident_id GROUP BY e1,e2";
+
+//Selezionare id,lat,lon di due eventi criminosi o meno avvenuti nello stesso giorno del mese dell'anno dello stesso tipo
+"SELECT e.incident_id AS e1,e.geo_lat AS lat1, e.geo_lon AS lon1, e2.geo_lat AS lat2, e2.geo_lon AS lon2 ,e2.incident_id AS e2 FROM events e , events e2 WHERE YEAR(e.reported_date)=? AND MONTH(e.reported_date)=? AND DAY(e.reported_date)=? AND YEAR(e2.reported_date)=? AND MONTH(e2.reported_date)=? AND DAY(e2.reported_date)=? AND e.offense_code/precinct_id/neighborhood_id/offense_type_id = e2.offense_code/precinct_id/neighborhood_id/offense_type_id AND e.incident_id <> e2.incident_id AND e.is_crime = 1/0 GROUP BY e1,e2"
