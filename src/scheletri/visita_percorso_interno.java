5d7ab3b9b009db382a03e6b45f424206b29a883e
Utente <Utente@192.168.1.2>
2020-07-14 13:04:53 +0200

    new

--- src/scheletri/visita_percorso_interno.java
+++ src/scheletri/visita_percorso_interno.java
@@ -92,4 +92,73 @@ public class visita_percorso_interno {
 			
 		}
 
+	
+	// finchè esistono archi
+	
+	public List<Actor>alberoVisita(Actor partenza) {
+		final Map<Actor,Actor> albero = new HashMap<>();
+		List<Actor> result = new ArrayList<Actor>();
+
+		
+		BreadthFirstIterator<Actor,DefaultWeightedEdge> it =new BreadthFirstIterator<>(this.grafo,partenza);
+		albero.put(partenza,null);
+		
+		it.addTraversalListener(new TraversalListener<Actor, DefaultWeightedEdge>() {
+			
+			@Override
+			public void vertexTraversed(VertexTraversalEvent<Actor> e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void vertexFinished(VertexTraversalEvent<Actor> e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void edgeTraversed(EdgeTraversalEvent<DefaultWeightedEdge> e) {
+				Actor sorgente= grafo.getEdgeSource(e.getEdge());
+				Actor destinazione= grafo.getEdgeTarget(e.getEdge());
+				if( !albero.containsKey(destinazione) && albero.containsKey(sorgente)) {
+					albero.put(destinazione, sorgente);
+				}else if( !albero.containsKey(sorgente) && albero.containsKey(destinazione)) {
+					albero.put(sorgente, destinazione);
+				}
+				
+			}
+			
+			@Override
+			public void connectedComponentStarted(ConnectedComponentTraversalEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void connectedComponentFinished(ConnectedComponentTraversalEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+
+			
+			
+	
+		while(it.hasNext()) {
+			it.next();
+		}
+		
+		for(Actor a:albero.keySet()) {
+			result.add(a);
+		}
+		result.remove(partenza);
+		
+		Collections.sort(result);
+
+
+		return result;
+	
+	}
+	
 }
